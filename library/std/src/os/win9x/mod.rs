632fdcdbc28ef0b2b935e9c4801eeba307e576a4
Hailey Somerville <hailey@hailey.lol>
2023-05-19 18:58:08 +1000

    add std::os::win9x mod

---
+++ library/std/src/os/win9x/mod.rs
@@ -0,0 +1,27 @@
+#![doc(cfg(target_os = "win9x"))]
+
+// pub mod ffi;
+// pub mod fs;
+// pub mod io;
+// pub mod process;
+// pub mod raw;
+// pub mod thread;
+
+/// A prelude for conveniently writing platform-specific code.
+///
+/// Includes all extension traits, and some important type definitions.
+pub mod prelude {
+//     #[doc(no_inline)]
+//     pub use super::ffi::{OsStrExt, OsStringExt};
+//     #[doc(no_inline)]
+//     pub use super::fs::FileExt;
+//     #[doc(no_inline)]
+//     pub use super::fs::{MetadataExt, OpenOptionsExt};
+//     #[doc(no_inline)]
+//     pub use super::io::{
+//         AsHandle, AsSocket, BorrowedHandle, BorrowedSocket, FromRawHandle, FromRawSocket,
+//         HandleOrInvalid, IntoRawHandle, IntoRawSocket, OwnedHandle, OwnedSocket,
+//     };
+//     #[doc(no_inline)]
+//     pub use super::io::{AsRawHandle, AsRawSocket, RawHandle, RawSocket};
+}
